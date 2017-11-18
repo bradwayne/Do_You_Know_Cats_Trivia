c4bffb40faf8875e4b8cff12df7bd9c8643b4711
unknown <bradwayne74@gmail.com>
2017-11-17 20:34:20 -0500

    game completion

---
+++ assets/javascript/app.js
@@ -0,0 +1,44 @@
+
+
+
+
+
+
+
+      
+
+
+
+function handleClick()
+  {         
+
+var correctAnswers = 0;
+
+var incorrectAnswers = 0;
+
+var unanswered = -37;
+
+
+
+for (var i = 0; i <= 30; i++) {
+
+  var radios = document.getElementsByName('question'+i);
+
+for (var j = 0; j < radios.length; j++) {
+
+  var radio = radios[j];
+    if (radio.value == "correct" && radio.checked) {
+          correctAnswers++;
+    }
+    else if (radio.value == "wrong" && radio.checked) {
+        incorrectAnswers++;
+    }   
+
+    else 
+        unanswered++;
+    
+  }
+ }                   
+    alert("Correct Answers: " + correctAnswers + "  Incorrect Answers: " + 
+      incorrectAnswers + "  Unanswered: " + unanswered);
+  }
