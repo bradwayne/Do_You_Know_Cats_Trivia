c61224952575021b66bb475518d1c77baba481ac
unknown <bradwayne74@gmail.com>
2017-11-29 16:33:46 -0500

    b.ground modify

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,35 +1,29 @@
+function handleClick() {
 
+    var correctAnswers = 0;
 
-function handleClick()
-  {         
+    var incorrectAnswers = 0;
 
-var correctAnswers = 0;
-
-var incorrectAnswers = 0;
-
-var unanswered = -37;
+    var unanswered = -37;
 
 
 
-for (var i = 0; i <= 30; i++) {
+    for (var i = 0; i <= 30; i++) {
 
-  var radios = document.getElementsByName('question'+i);
+        var radios = document.getElementsByName('question' + i);
 
-for (var j = 0; j < radios.length; j++) {
+        for (var j = 0; j < radios.length; j++) {
 
-  var radio = radios[j];
-    if (radio.value == "correct" && radio.checked) {
-          correctAnswers++;
+            var radio = radios[j];
+            if (radio.value == "correct" && radio.checked) {
+                correctAnswers++;
+            } else if (radio.value == "wrong" && radio.checked) {
+                incorrectAnswers++;
+            } else
+                unanswered++;
+
+        }
     }
-    else if (radio.value == "wrong" && radio.checked) {
-        incorrectAnswers++;
-    }   
-
-    else 
-        unanswered++;
-    
-  }
- }                   
-    alert("Correct Answers: " + correctAnswers + "  Incorrect Answers: " + 
-      incorrectAnswers + "  Unanswered: " + unanswered);
-  }
+    alert("Correct Answers: " + correctAnswers + "  Incorrect Answers: " +
+        incorrectAnswers + "  Unanswered: " + unanswered);
+}
