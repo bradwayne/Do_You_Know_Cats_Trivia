54cce6add7ed7feb2c4dbb436ff0f5332091b3b8
unknown <bradwayne74@gmail.com>
2019-02-26 12:13:42 -0500

    modified background image to full screen

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -28,98 +28,100 @@ var panel = $("#quiz-area");
 var countStartNumber = 30;
 
 var questions = [{
-        question: "Which brain is a cat's brain most similar to?",
-        answers: ["Human", "Dog"],
-        correctAnswer: "Human",
-        image: "assets/images/cat_hitting_pillow.gif"
-    },
+    question: "Which brain is a cat's brain most similar to?",
+    answers: ["Human", "Dog"],
+    correctAnswer: "Human",
+    image: "assets/images/cat_hitting_pillow.gif"
+},
 
-    {
-        question: "Term for a group of cats?",
-        answers: ["Caggle", "Clowder", "Covey", "Clutch"],
-        correctAnswer: "Clowder",
-        image: "assets/images/clouder.gif"
-    },
+{
+    question: "Term for a group of cats?",
+    answers: ["Caggle", "Clowder", "Covey", "Clutch"],
+    correctAnswer: "Clowder",
+    image: "assets/images/clouder.gif"
+},
 
-    {
-        question: "A cat has how many whiskers, on average?",
-        answers: ["16", "12", "8", "24"],
-        correctAnswer: "24",
-        image: "assets/images/whiskers.gif"
-    },
+{
+    question: "A cat has how many whiskers, on average?",
+    answers: ["16", "12", "8", "24"],
+    correctAnswer: "24",
+    image: "assets/images/whiskers.gif"
+},
 
-    {
-        question: "Do cats have fewer or more teeth than dogs?",
-        answers: ["Fewer", "More"],
-        correctAnswer: "Fewer",
-        image: "assets/images/dualing_yawn.gif"
-    },
+{
+    question: "Do cats have fewer or more teeth than dogs?",
+    answers: ["Fewer", "More"],
+    correctAnswer: "Fewer",
+    image: "assets/images/dualing_yawn.gif"
+},
 
-    {
-        question: "Thick hair around the face of some cats (Persians) is called:",
-        answers: ["Mane", "Shock", "Ruff", "Pelt"],
-        correctAnswer: "Ruff",
-        image: "assets/images/ruff.gif"
-    },
+{
+    question: "Thick hair around the face of some cats (Persians) is called:",
+    answers: ["Mane", "Shock", "Ruff", "Pelt"],
+    correctAnswer: "Ruff",
+    image: "assets/images/ruff.gif"
+},
 
-    {
-        question: "A cat rubbing the side of its head on you or furniture is called:",
-        answers: ["Bunting", "Beaning", "Brocking", "Tagging"],
-        correctAnswer: "Bunting",
-        image: "assets/images/rubbing.gif"
-    },
+{
+    question: "A cat rubbing the side of its head on you or furniture is called:",
+    answers: ["Bunting", "Beaning", "Brocking", "Tagging"],
+    correctAnswer: "Bunting",
+    image: "assets/images/rubbing.gif"
+},
 
-    {
-        question: "Outdoor-only cats live, on average, about:",
-        answers: ["7 to 10 years", "3 - 5 years", "12 - 15 years"],
-        correctAnswer: "3 - 5 years",
-        image: "assets/images/outdoors.gif"
-    },
+{
+    question: "Outdoor-only cats live, on average, about:",
+    answers: ["7 to 10 years", "3 - 5 years", "12 - 15 years"],
+    correctAnswer: "3 - 5 years",
+    image: "assets/images/outdoors.gif"
+},
 
-    {
-        question: "'Tabby' is a name of a breed.",
-        answers: ["False", "True"],
-        correctAnswer: "False",
-        image: "assets/images/pumping_fist.gif"
-    },
+{
+    question: "'Tabby' is a name of a breed.",
+    answers: ["False", "True"],
+    correctAnswer: "False",
+    image: "assets/images/pumping_fist.gif"
+},
 
-    {
-        question: "Term for a group of kittens is called:",
-        answers: ["Nook", "Kaggle", "Kaboodle", "Kindle"],
-        correctAnswer: "Kindle",
-        image: "assets/images/kindle.gif"
-    },
+{
+    question: "Term for a group of kittens is called:",
+    answers: ["Nook", "Kaggle", "Kaboodle", "Kindle"],
+    correctAnswer: "Kindle",
+    image: "assets/images/kindle.gif"
+},
 
-    {
-        question: "A cat can jump how many times its height?",
-        answers: ["4", "6", "2", "8"],
-        correctAnswer: "6",
-        image: "assets/images/jumping.webp"
-    },
+{
+    question: "A cat can jump how many times its height?",
+    answers: ["4", "6", "2", "8"],
+    correctAnswer: "6",
+    image: "assets/images/jumping.webp"
+},
 
-    {
-        question: "All are names of cat breeds, except:",
-        answers: ["Birman", "Beauceron", "Balinese", "Burmilla"],
-        correctAnswer: "Beauceron",
-        image: "assets/images/blow_dryer.gif"
-    },
+{
+    question: "All are names of cat breeds, except:",
+    answers: ["Birman", "Beauceron", "Balinese", "Burmilla"],
+    correctAnswer: "Beauceron",
+    image: "assets/images/blow_dryer.gif"
+},
 
-    {
-        question: "Cats can't taste this:",
-        answers: ["Bitter", "Sour", "Salt", "Sweet"],
-        correctAnswer: "Sweet",
-        image: "assets/images/tasting.webp"
-    },
+{
+    question: "Cats can't taste this:",
+    answers: ["Bitter", "Sour", "Salt", "Sweet"],
+    correctAnswer: "Sweet",
+    image: "assets/images/tasting.webp"
+},
 
-    {
-        question: "Which statements about cat body language is FALSE?",
-        answers: ["A tail tucked underneath body means I am hungry.",
+{
+    question: "Which statements about cat body language is FALSE?",
+    answers: ["A tail tucked underneath body means I am hungry.",
         "A tail twitching means I am getting irritated.",
         "A tail thumping means I am totally frustrated.",
-        "A tail held high means I am happy."],
-        correctAnswer: "A tail tucked underneath body means I am hungry.",
-        image: "assets/images/winking.gif"
-    }];
+        "A tail held high means I am happy."
+    ],
+    correctAnswer: "A tail tucked underneath body means I am hungry.",
+    image: "assets/images/winking.gif"
+}
+];
 
 
 var timer;
@@ -167,36 +169,13 @@ var game = {
         $("#counter-number").html(game.counter);
 
         panel.html("<h2>Out of Time!</h2>");
-        panel.append("<h3>The Correct Answer was: " + questions[this.currentQuestion].correctAnswer);
-        panel.append("<img src='" + questions[this.currentQuestion].image + "' />");
+        panel.append("<h3>The Correct Answer was: " + questions[game.currentQuestion].correctAnswer + "</h3>");
+        panel.append("<img src='" + questions[game.currentQuestion].image + "' />");
 
         if (game.currentQuestion === questions.length - 1) {
             setTimeout(game.results, 3 * 1000);
         } else {
             setTimeout(game.nextQuestion, 3 * 1000);
-        }
-    },
-
-    results: function () {
-
-        clearInterval(timer);
-
-        panel.html("<h2>All done, heres how you did!</h2>");
-
-        $("#counter-number").text(game.counter);
-
-        panel.append("<h3>Correct Answers: " + game.correct + "</h3>");
-        panel.append("<h3>Incorrect Answers: " + game.incorrect + "</h3>");
-        panel.append("<h3>Unanswered: " + (questions.length - (game.incorrect + game.correct)) + "</h3>");
-        panel.append("<br><button class='btn' id='start-over'>Start Over ?</button>");
-    },
-
-    clicked: function (e) {
-        clearInterval(timer);
-        if ($(e.target).attr("data-name") === questions[this.currentQuestion].correctAnswer) {
-            this.answeredCorrectly();
-        } else {
-            this.answeredIncorrectly();
         }
     },
 
@@ -233,6 +212,29 @@ var game = {
         }
     },
 
+    results: function () {
+
+        clearInterval(timer);
+
+        panel.html("<h2>All done, here's how you did!</h2>");
+
+        $("#counter-number").text(game.counter);
+
+        panel.append("<h3>Correct Answers: " + game.correct + "</h3>");
+        panel.append("<h3>Incorrect Answers: " + game.incorrect + "</h3>");
+        panel.append("<h3>Unanswered: " + (questions.length - (game.incorrect + game.correct)) + "</h3>");
+        panel.append("<br><button class='btn' id='start-over'>Start Over ?</button>");
+    },
+
+    clicked: function (e) {
+        clearInterval(timer);
+        if ($(e.target).attr("data-name") === questions[this.currentQuestion].correctAnswer) {
+            this.answeredCorrectly();
+        } else {
+            this.answeredIncorrectly();
+        }
+    },
+
     reset: function () {
         this.currentQuestion = 0;
         this.counter = countStartNumber;
@@ -243,16 +245,15 @@ var game = {
 };
 
 // CLICK EVENTS
-
-$(document).on("click", "#start-over", function () {
-    game.reset();
+$(document).on("click", "#start", function () {
+    $("#sub-wrapper").prepend("<h2 id='time'>Question Time Remaining: <span id='counter-number'>30</span> Seconds</h2>");
+    game.loadQuestion();
 });
 
 $(document).on("click", ".answer-button", function (e) {
     game.clicked(e);
 });
 
-$(document).on("click", "#start", function () {
-    $("#sub-wrapper").prepend("<h2 id='time'>Question Time Remaining: <span id='counter-number'>30</span> Seconds</h2>");
-    game.loadQuestion();
+$(document).on("click", "#start-over", function () {
+    game.reset();
 });
